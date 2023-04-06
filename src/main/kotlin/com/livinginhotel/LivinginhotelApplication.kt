fc5fcf9693a16538134c74ccad3f8c76b0ce331f
kai <junwoo.ahn@fitpet.co.kr>
2023-04-06 15:10:38 +0900

    init

---
+++ src/main/kotlin/com/livinginhotel/LivinginhotelApplication.kt
@@ -0,0 +1,11 @@
+package com.livinginhotel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LivinginhotelApplication
+
+fun main(args: Array<String>) {
+    runApplication<LivinginhotelApplication>(*args)
+}
