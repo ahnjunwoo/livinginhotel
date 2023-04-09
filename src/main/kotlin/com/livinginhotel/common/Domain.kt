9d36e01fd3085b5c64e7bfd797aa19a43ca61d99
kai <junwoo.ahn@fitpet.co.kr>
2023-04-09 17:27:30 +0900

    어노테이션 처리

---
+++ src/main/kotlin/com/livinginhotel/common/Domain.kt
@@ -0,0 +1,3 @@
+package com.livinginhotel.common
+
+annotation class Domain()
---
+++ src/main/kotlin/com/livinginhotel/common/PersistenceAdapter.kt
@@ -0,0 +1,14 @@
+package com.livinginhotel.common
+
+import org.springframework.core.annotation.AliasFor
+import org.springframework.stereotype.Component
+
+@Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+@Component
+annotation class PersistenceAdapter(
+
+    @get:AliasFor(annotation = Component::class)
+    val value: String = ""
+)
